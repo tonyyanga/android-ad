31d6b0323a11eb5476b3f394104e0b218395842a
Tony Yang <tony@ycyang.me>
2017-03-19 16:12:57 -0700

    support doubleclick ads?

--- mitm-scripts/print_ad_dest.py
+++ mitm-scripts/print_ad_dest.py
@@ -9,22 +9,29 @@ count = 0
 
 class print_ad_dest:
 
+    def __init__(self):
+        # primarily used for DataXu urls (https://i.w55c.net/*)
+        self.regex1 = re.compile('rurl%3Dhttp.*?\\\\x26')
+        self.regex2 = re.compile('adurl=http.*?\"')
+        self.regex3 = re.compile("adurl\\\\x3dhttp.*?(?:\'|\"|\\\\x22)")
+
     def response(self, flow):
         global count
         try:
-            if (flow.request.headers['Host'] == "pubads.g.doubleclick.net" or
-                    "https://googleads.g.doubleclick.net/mads/gma" in flow.request.pretty_url) and len(flow.response.content) > 0:
+            if ("https://pubads.g.doubleclick.net/gampad/ads" in flow.request.pretty_url or
+                    "https://googleads.g.doubleclick.net/mads/gma" in flow.request.pretty_url or
+                    "https://ad.doubleclick.net/ddm/adj" in flow.request.pretty_url) \
+                    and len(flow.response.content) > 0:
                 print(
                     flow.request.method + " " + urllib.unquote(flow.request.pretty_url) + "\n")
                 raw_html = flow.response.content
                 soup = BeautifulSoup(
                     raw_html.replace("\\x3d", "=").replace("\\x22", '"'))
-                m1 = re.search('buildAdSlot\(.*?\)', raw_html)
-                m2 = re.search('adurl=http.*?\"', raw_html)
-                m3 = re.search(
-                    "adurl\\\\x3dhttp.*?(?:\'|\"|\\\\x22)", raw_html)
+                m1 = self.regex1.search(raw_html)
+                m2 = self.regex2.search(raw_html)
+                m3 = self.regex3.search(raw_html)
                 if m1:
-                    print(m1.group(0) + '\n')
+                    print(urllib.unquote(m1.group(0)[7:-4]) + '\n')
                 elif m2:
                     print(urllib.unquote(m2.group(0)[6:-1]) + '\n')
                 elif m3:
@@ -38,7 +45,7 @@ class print_ad_dest:
                     print("NO MATCH!!!!\n")
                     with open("/tmp/" + str(count) + ".html.txt", "w") as f:
                         f.write(
-                            "<!-- Original url: " + flow.request.pretty_url + "-->\n")
+                            "<!-- Original url: " + flow.request.pretty_url + " -->\n")
                         f.write(soup.prettify())
                     count += 1
         except KeyError:
